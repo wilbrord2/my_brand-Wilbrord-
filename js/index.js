039eee22c825a17029758f5a6abeed4c90c1d44d
wilbrord2 <bwilbrord@gmail.com>
2022-12-07 17:16:51 +0200

    responsive home page

---
+++ js/index.js
@@ -0,0 +1,15 @@
+"use strick";
+
+const menuBtn = document.getElementById("menuBtn");
+const menilist = document.getElementById("menulist");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", function () {
+  menilist.classList.remove("hidde");
+  menilist.classList.add("navbarAnimation");
+  //   console.log("remove hidden");
+});
+closeBtn.addEventListener("click", function () {
+  menilist.classList.add("hidde");
+  //   console.log("add hidde");
+});
